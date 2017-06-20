8ab23c9d23f9b5134fe5a51b9b384f9e293a9e52
Ricky Dunlop <hello@rickydunlop.co.uk>
2017-06-20 17:44:18 +0100

    Changed url paths into query params and fixed some error handling

--- controllers/images.js
+++ controllers/images.js
@@ -35,18 +35,11 @@ class ImageController {
    * @param {ctx} Koa Context
    */
   static async findById(ctx) {
-    try {
-      const image = await Image.findOne({ id: ctx.params.id });
-      if (!image) {
-        ctx.throw(404);
-      }
-      ctx.body = image;
-    } catch (err) {
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
+    const image = await Image.findOne({ id: ctx.params.id });
+    if (!image) {
+      ctx.throw(404);
     }
+    ctx.body = image;
   }
 
   /**
@@ -54,21 +47,16 @@ class ImageController {
    * @param {ctx} Koa Context
    */
   static async add(ctx) {
-    try {
-      const image = ctx.request.body.files.image;
-      const fileName = path.basename(image.path);
-      const dimensions = sizeOf(image.path);
-      const imageData = {
-        name: fileName,
-        url: `/uploads/${fileName}`,
-        width: dimensions.width,
-        height: dimensions.height,
-      };
-      ctx.body = await new Image(imageData).save();
-    } catch (err) {
-      console.log(err);
-      ctx.throw(500);
-    }
+    const image = ctx.request.body.files.image;
+    const fileName = path.basename(image.path);
+    const dimensions = sizeOf(image.path);
+    const imageData = {
+      name: fileName,
+      url: `/uploads/${fileName}`,
+      width: dimensions.width,
+      height: dimensions.height,
+    };
+    ctx.body = await new Image(imageData).save();
   }
 
   /**
@@ -76,18 +64,11 @@ class ImageController {
    * @param {ctx} Koa Context
    */
   static async delete(ctx) {
-    try {
-      const image = await Image.findOne({ id: ctx.params.id }).remove();
-      if (!image) {
-        ctx.throw(404);
-      }
-      ctx.body = image;
-    } catch (err) {
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
+    const image = await Image.findOne({ id: ctx.params.id }).remove();
+    if (!image) {
+      ctx.throw(404);
     }
+    ctx.body = image;
   }
 
   /**
@@ -95,43 +76,46 @@ class ImageController {
    * @param {ctx} Koa Context
    */
   static async resize(ctx) {
-    try {
-      const image = await Image.findOne({ id: ctx.params.id });
-      if (!image) {
-        ctx.throw(404);
-      }
-      // Check to see if the rendition already exists
-      const width = parseInt(ctx.params.width, 10);
-      const height = parseInt(ctx.params.height, 10);
-      const params = `${width}x${height}`;
-      const rendition = image.renditions.filter(elem => elem.params === params);
-      if (rendition.length) {
-        ctx.body = rendition;
-      } else {
-        // Create a new rendition and save it
-        const img = path.join(__dirname, '../public/uploads/', image.name);
-        const newFilePath = getNewFilePath(img, params);
-        const info = await Sharp(img)
-          .resize(width, height)
-          .toFile(newFilePath);
+    const image = await Image.findOne({ id: ctx.params.id });
+    if (!image) {
+      ctx.throw(404);
+    }
 
-        const newRendition = {
-          params,
-          name: newFilePath,
-          url: newFilePath,
-          width: info.width,
-          height: info.height,
-        };
-        image.renditions.push(newRendition);
-        await image.save();
-        ctx.body = newRendition;
-      }
-    } catch (err) {
-      console.log(err);
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
+    const qry = ctx.request.query;
+    if (!('width' in qry)) {
+      ctx.throw(422, 'width parameter is required');
+    }
+
+    if (!('height' in qry)) {
+      ctx.throw(422, 'height parameter is required');
+    }
+
+    const width = parseInt(qry.height, 10);
+    const height = parseInt(qry.height, 10);
+    const params = `${width}x${height}`;
+
+    // Check to see if the rendition already exists
+    const rendition = image.renditions.filter(elem => elem.params === params);
+    if (rendition.length) {
+      ctx.body = rendition;
+    } else {
+      // Create a new rendition and save it
+      const img = path.join(__dirname, '../public/uploads/', image.name);
+      const newFilePath = getNewFilePath(img, params);
+      const info = await Sharp(img)
+        .resize(width, height)
+        .toFile(newFilePath);
+
+      const newRendition = {
+        params,
+        name: newFilePath,
+        url: newFilePath,
+        width: info.width,
+        height: info.height,
+      };
+      image.renditions.push(newRendition);
+      await image.save();
+      ctx.body = newRendition;
     }
   }
 
@@ -140,42 +124,43 @@ class ImageController {
    * @param {ctx} Koa Context
    */
   static async rotate(ctx) {
-    try {
-      const image = await Image.findOne({ id: ctx.params.id });
-      if (!image) {
-        ctx.throw(404);
-      }
-      // Check to see if the rendition already exists
-      const angle = parseInt(ctx.params.angle, 10);
-      const params = `r${angle}`;
-      const rendition = image.renditions.filter(elem => elem.params === params);
-      if (rendition.length) {
-        ctx.body = rendition;
-      } else {
-        // Create a new rendition and save it
-        const img = path.join(__dirname, '../public/uploads/', image.name);
-        const newFilePath = getNewFilePath(img, params);
-        const info = await Sharp(img)
-          .rotate(angle)
-          .toFile(newFilePath);
+    const image = await Image.findOne({ id: ctx.params.id });
+    if (!image) {
+      ctx.throw(404);
+    }
+    const qry = ctx.request.query;
+    if (!('angle' in qry)) {
+      ctx.throw(422, 'angle parameter is required');
+    }
 
-        const newRendition = {
-          params,
-          name: newFilePath,
-          url: newFilePath,
-          width: info.width,
-          height: info.height,
-        };
-        image.renditions.push(newRendition);
-        await image.save();
-        ctx.body = newRendition;
-      }
-    } catch (err) {
-      console.log(err);
-      if (err.name === 'CastError' || err.name === 'NotFoundError') {
-        ctx.throw(404);
-      }
-      ctx.throw(500);
+    // Check to see if the rendition already exists
+    const angle = parseInt(qry.angle, 10);
+    if (angle % 90 !== 0) {
+      ctx.throw(422, 'angle must be a multiple of 90');
+    }
+
+    const params = `r${angle}`;
+    const rendition = image.renditions.filter(elem => elem.params === params);
+    if (rendition.length) {
+      ctx.body = rendition;
+    } else {
+      // Create a new rendition and save it
+      const img = path.join(__dirname, '../public/uploads/', image.name);
+      const newFilePath = getNewFilePath(img, params);
+      const info = await Sharp(img)
+        .rotate(angle)
+        .toFile(newFilePath);
+
+      const newRendition = {
+        params,
+        name: newFilePath,
+        url: newFilePath,
+        width: info.width,
+        height: info.height,
+      };
+      image.renditions.push(newRendition);
+      await image.save();
+      ctx.body = newRendition;
     }
   }
   /* eslint-enable no-param-reassign */
