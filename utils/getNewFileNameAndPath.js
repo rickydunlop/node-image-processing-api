4f2825b61a8e9e6824ec56c276b9732df21d876b
Ricky Dunlop <hello@rickydunlop.co.uk>
2017-06-20 17:58:05 +0100

    Fixed the path and filename for transformed images

--- utils/getNewFileNameAndPath.js
+++ utils/getNewFileNameAndPath.js
@@ -3,7 +3,7 @@ import path from 'path';
 export default (img, params) => {
   const fileExtension = path.extname(img);
   const fileName = path.basename(img, fileExtension);
-  const newFileName = `${fileName}-${params}.${fileExtension}`;
+  const newFileName = `${fileName}-${params}${fileExtension}`;
   const newFilePath = path.join(__dirname, '../public/uploads/', newFileName);
-  return newFilePath;
+  return { newFileName, newFilePath };
 };
